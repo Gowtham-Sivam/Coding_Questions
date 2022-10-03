9c8f067b0521d75e3eff14494d157cdb497dff88
Gowtham <gowthamsivams@gmail.com>
2022-10-04 02:09:48 +0530

    Solution - Arithmetic Coding Prepbytes

--- Arithmetic_Progression.cpp
+++ Arithmetic_Progression.cpp
@@ -1,10 +1,47 @@
-#include<iostream>
-using namespace  std ;
+#include <bits/stdc++.h>
+#include<map>
 
-int main ()
-{
-    int n ;
-    n = 5 ;
-    cout << n ;
-    return 0 ;
+using namespace std;
+
+int main() {
+    map<int, vector<int>> element_index_map;
+    int n;
+    cin >> n;
+    int i, val;
+    for (i = 0; i < n; i++) {
+        cin >> val;
+        element_index_map[val].push_back(i);
+    }
+    int vec_ele, diff, diff1;
+
+    cout << element_index_map.size() << endl ;
+    for (map<int, vector<int>>::iterator it = element_index_map.begin(); it != element_index_map.end(); ++it) {
+        if (it->second.size() == 1)
+            cout << (*it).first << " " << "0" << endl;
+
+        else if (it->second.size() == 2)
+            cout << (*it).first << " " << it->second[1] - it->second[0] << endl ;
+
+        else if (it->second.size() > 2){
+                bool flag = 0;
+                vec_ele = it->second[0];
+                diff = abs(it->second[1] - it->second[0]);
+                for (i =  1 ; i < it -> second.size() ; i++) {
+                   diff1 = abs (vec_ele - it->second[i] )  ;
+                   if (diff == diff1){
+                       flag = 1 ;
+                   }else{
+                       flag = 0 ;
+                   }
+                   vec_ele = it->second[i] ;
+                }
+                if (flag){
+                    cout << (*it).first << " " << diff1 << endl  ;
+                 }
+            }
+
+
+    }
+
+    return 0;
 }
