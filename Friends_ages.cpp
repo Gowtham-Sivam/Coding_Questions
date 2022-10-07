b72104775c27927cd287e45056c2312f0689cdf1
Gowtham <gowthamsivams@gmail.com>
2022-10-07 10:00:52 +0530

    Friends_ages_prepbytes_Bad_Question

--- Friends_ages.cpp
+++ Friends_ages.cpp
@@ -6,25 +6,26 @@ using namespace std;
 void friends(int arr[], int n) {
     int i = 0, j = 0;
     int count = 0;
-    map<int, int> mp;
+    map<int, vector<int>> mp;
 
 
     for (i = 0; i < n; i++) {
-        for (j = 0 ; j < n; j++) {
+        for (j = i+1; j < n; j++) {
 
             if ((arr[j] <= 0.5 * arr[i] + 7) || (arr[j] > 100 && arr[i] < 100) || (arr[j] > arr[i])) {
-                continue;
-            } else if ((mp[i] == j) || (mp[j] == i)) {
+                continue ;
+            } else if ((find(mp[arr[i]].begin(), mp[arr[i]].end(), arr[j]) != mp[arr[i]].end()) ||
+                       (find(mp[arr[j]].begin(), mp[arr[j]].end(), arr[i]) != mp[arr[j]].end())) {
                 continue;
             } else {
-                mp[i] = j;
-                mp[j] = i;
-//                cout << arr[i] << " " << arr[j] << endl ;
+                mp[arr[i]].push_back(arr[j]);
+                mp[arr[j]].push_back(arr[i]);
+                cout << arr[i] << " " << arr[j] << endl ;
                 count++;
             }
         }
     }
-    cout << count  << endl;
+    cout << count << endl;
 }
 
 int main() {
@@ -43,4 +44,3 @@ int main() {
     return 0;
 }
 
-https://mycode.prepbytes.com/problems/arrays/FRINDSAGE
